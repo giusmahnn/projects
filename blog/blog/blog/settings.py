a7d1425bd8be8e209d512849b5efcfc444d7f507
giusmahn <garethremigius@gmail.com>
2023-05-29 19:46:55 +0100

    .

--- blog/blog/blog/settings.py
+++ blog/blog/blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts'
+    'posts',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+
+LOGIN_REDIRECT = 'index'
+LOGOUT_REDIRECT = 'index'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
